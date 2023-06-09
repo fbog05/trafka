4011b4dbe1846e9e638e153d8c4dee39c82268d3
Németh Ottó <ottoburg17@gamil.com>
2023-06-09 10:18:19 +0200

    Kész

---
+++ App.js
@@ -0,0 +1,58 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+
+export default function App() {
+
+  const [count, setCount] = useState(0)
+
+  function increment(){
+    setCount(count+1)
+  }
+
+  function reset(){
+    setCount(0)
+  }
+
+  function decrement(){
+    if(count > 0)
+    setCount(count-1)
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Számláló
+      </Text>
+
+      <Text style={styles.count}>{count}</Text>
+
+      <Button title="Növel" onPress={increment}/>
+      <Button title="Reset" onPress={reset}/>
+      <Button title="Csökkent" onPress={decrement}/>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'navy',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title: {
+    color: 'white',
+    fontSize: 32,
+  },
+
+  count: {
+    color: 'yellow',
+    fontSize: 44,
+  },
+
+});
